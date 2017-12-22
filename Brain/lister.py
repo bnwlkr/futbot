6e80bdc12b4af972e3e39d62340f268d814eb0d5
bnwlkr <bnwlkr@icloud.com>
2017-12-21 19:53:42 -0800

    more detailed listing process

--- Brain/lister.py
+++ Brain/lister.py
@@ -7,23 +7,36 @@ import fut
 class Lister:  ##TODO: List items at the right price
     @classmethod
     def list(cls, session):
-        for item in session.unassigned():
-            session.sendToTradepile(item['id'])
-        for item in session.tradepile():
-            if item['tradeState'] is None:
-                if item['itemType'] == 'player':
-                    Lister.listPlayer(item, session)
-                    print('item listed!')
-                elif item['itemType'] == 'contract' or item['itemType'] == 'health':
-                    session.sell(item['id'], 150, 200)
-                elif item['itemType'] == 'kit' or item['itemType'] == 'ball':
-                    session.quickSell(item['id'])
-                else:
-                    print ("What should I list this item at?")
-                    print item
-                    userinput = int(input("Start price: "))
-                    session.sell(item['id'], userinput, userinput + 100)
-                    print('item listed!')
+        try:
+            for item in session.unassigned():
+                session.sendToTradepile(item['id'])
+            for item in session.tradepile():
+                if item['tradeState'] is None:
+                    if item['itemType'] == 'player':
+                        Lister.listPlayer(item, session)
+                        print('item listed!')
+                    elif item['itemType'] == 'contract' or item['itemType'] == 'health' or item['itemType'] == 'manager':
+                        session.sell(item['id'], 150, 200)
+                    elif item['itemType'] == 'training':
+                        if item['rareflag'] == 0:
+                            session.sell(item['id'], 150, 200)
+                        else:
+                            session.sell(item['id'], 300, 400)
+                    elif item['itemType'] == 'kit' or item['itemType'] == 'ball' or item['itemType'] == 'gkCoach':
+                        session.quickSell(item['id'])
+                    else:
+                        print ("What should I list this item at?")
+                        print item
+                        userinput = int(input("Start price: "))
+                        session.sell(item['id'], userinput, userinput + 100)
+                        print('item listed!')
+        except fut.PermissionDenied:
+            print item
+
+        except fut.UnknownError:
+            print("I couldn't list this item. You figure it out, conscious human.")
+            print(item)
+            print "price: " + str(item)
 
 
     @classmethod
@@ -32,21 +45,16 @@ class Lister:  ##TODO: List items at the right price
 
     @classmethod
     def listPlayer(cls, player, session):
-        try:
             price = Scout.marketpricing(session, player['assetId'])
             if player['lastSalePrice'] == 0: # this player was packed
                 if price == 10000:
                     session.sell(player['id'], 150, 200)
+                    print('player listed!')
                 else:
                     listprice = max(player['marketDataMinPrice'], price - 100)
                     session.sell(player['id'], listprice, listprice + 100)
                 print('player listed!')
-        except fut.PermissionDenied:
-            print player
-        except fut.UnknownError:
-            print("I couldn't list this player. You figure it out, conscious human.")
-            print(player)
-            print "price: " + str(price)
+
 
 
 
